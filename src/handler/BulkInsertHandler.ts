1c12e5a2f85e800bdae6d3e204401ed66f14b51c
Jeffey <ref6318@gmail.com>
2022-08-12 22:00:24 +0900

    [ADD]: handler

---
+++ src/handler/BulkInsertHandler.ts
@@ -0,0 +1,15 @@
+class BulkInsertHanlder {
+    private dummData: Array<{title: string, desc: string}>
+
+    constructor() {
+        this.dummData = [];
+    }
+
+    pushDummyData() {
+
+    }
+
+    bulkInsert() {
+
+    }
+}
